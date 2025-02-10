c7c87c319ee9ca206ffd94bf5c9fb5ede19c90f8
chanchaltharwani <chanchaltharwani80@gmail.com>
2025-02-10 19:06:35 +0530

    Generate chat gpt api key from Open Api platform and Api key added in global.dart  file as a const type of variable

--- lib/helper/global.dart
+++ lib/helper/global.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 const appName = "Ai Express";
 
 late Size mq;
+const apiKey =
+    "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";
